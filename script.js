52e72a9ff5f6eeedd62ce580c58008e490ec5c30
JustKyrix <kyrixfx@outlook.de>
2021-01-14 20:39:08 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var counter = 0;
+var colors = ["#ff0000", "#ff7f00", "#ffff00", "#00ff00", "#00ffff", "#0000ff", "#8b00ff"];
+
+function setup() {
+    setInterval(changeColor, 1500);
+}
+
+function changeColor() {
+    var body = document.body;
+    body.style.backgroundColor = colors[counter];
+
+    if(counter == 6) {
+        counter = 0;
+    } else {
+        counter++;
+    }
+}
